40c67d4db105ced26fd3f669d7080cc398f26cc1
Alex Bespalov <alexbespalow@yandex.ru>
2024-07-02 20:02:38 +0300

    Objects and Methods

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int price = 12000;
-        System.out.println("Стоимость билета = " + price);
-        int bonus = 20;
-        System.out.println("За каждые " + bonus + " рублей потраченные на билет, начисляется 1 бонус");
-        int totalBonus = price / bonus;
-        System.out.println("Итого бонусов = " + totalBonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
